c1726576d11b2a10c10b41b938c943291e2857d3
precondition <57645186+precondition@users.noreply.github.com>
2026-01-24 22:50:41 +0100

    Re-use code from dconf to sort output
    The program is now a lot slower but the output is byte-for-byte equal to
    `dconf dump /`

--- gvdb/gvdb-dumper.c
+++ gvdb/gvdb-dumper.c
@@ -23,6 +23,116 @@ add_key (GKeyFile *kf, const gchar *full_key, GVariant *value)
     g_variant_unref(value);
 }
 
+
+/**
+ * Comparison function for paths that orders keys before dirs.
+ */
+static gint
+path_compare (const void *a,
+              const void *b)
+{
+  const gchar *as = *(const gchar **)a;
+  const gchar *bs = *(const gchar **)b;
+
+  const gboolean a_is_dir = !!g_str_has_suffix (as, "/");
+  const gboolean b_is_dir = !!g_str_has_suffix (bs, "/");
+
+  if (a_is_dir != b_is_dir)
+    return a_is_dir - b_is_dir;
+  else
+    return strcmp (as, bs);
+}
+
+static gchar **
+gvdb_list_dir (GvdbTable *table,
+               const gchar *dir,
+               gint *out_len)
+{
+    gchar **names = gvdb_table_get_names (table, NULL);
+    GPtrArray *items = g_ptr_array_new_with_free_func (g_free);
+    gsize dir_len = strlen (dir);
+
+    for (gchar **n = names; *n; n++) {
+        const gchar *path = *n;
+
+        if (!g_str_has_prefix (path, dir))
+            continue;
+
+        const gchar *rest = path + dir_len;
+        if (*rest == '\0')
+            continue;
+
+        const gchar *slash = strchr (rest, '/');
+
+        if (slash) {
+            /* directory */
+            gsize len = slash - rest + 1;
+            gchar *entry = g_strndup (rest, len);
+            g_ptr_array_add (items, entry);
+        } else {
+            /* key */
+            g_ptr_array_add (items, g_strdup (rest));
+        }
+    }
+
+    g_strfreev (names);
+
+    g_ptr_array_sort (items, path_compare);
+    g_ptr_array_add (items, NULL);
+
+    if (out_len)
+        *out_len = items->len - 1;
+
+    return (gchar **) g_ptr_array_free (items, FALSE);
+}
+
+static void
+add_to_keyfile (GKeyFile    *kf,
+                GvdbTable *table,
+                const gchar *dir_src,
+                const gchar *dir_dst)
+{
+  g_autofree gchar *group = NULL;
+  g_auto(GStrv) items = NULL;
+  gint length;
+  gsize n;
+
+  /* Key-file group names are formed by removing initial and trailing slash
+   * from dir name, with the singular exception of root dir whose group name
+   * is just "/". */
+
+  n = strlen (dir_dst);
+  g_assert (n >= 1 && dir_dst[n - 1] == '/');
+
+  if (g_str_equal (dir_dst, "/"))
+    group = g_strdup ("/");
+  else
+    group = g_strndup (dir_dst + 1, n - 2);
+
+  items = gvdb_list_dir(table, dir_src, &length);
+  qsort (items, length, sizeof (items[0]), path_compare);
+
+  for (gchar **item = items; *item; ++item)
+    {
+      g_autofree gchar *path = g_strconcat (dir_src, *item, NULL);
+
+      if (g_str_has_suffix (*item, "/"))
+        {
+          g_autofree gchar *subdir = g_strconcat (dir_dst, *item, NULL);
+          add_to_keyfile (kf, table, path, subdir);
+        }
+      else
+        {
+          g_autoptr(GVariant) value = gvdb_table_get_value (table, path);
+          if (value != NULL)
+            {
+              g_autofree gchar *value_str = g_variant_print (value, TRUE);
+              g_key_file_set_value (kf, group, *item, value_str);
+            }
+        }
+    }
+}
+
 int
 main (int argc, char **argv)
 {
@@ -41,21 +151,8 @@ main (int argc, char **argv)
 
     GKeyFile *kf = g_key_file_new();
 
-    gchar **names = gvdb_table_get_names(table, NULL);
-    for (gchar **n = names; *n; n++) {
-        /* dconf keys are always absolute paths */
-        if (**n != '/')
-            continue;
+    add_to_keyfile (kf, table, "/", "/");
 
-        GVariant *value = gvdb_table_get_value(table, *n);
-        if (!value)
-            continue;
-
-        add_key(kf, *n, value);
-        g_variant_unref(value);
-    }
-
-    g_strfreev(names);
     gvdb_table_free(table);
 
     gsize length;
